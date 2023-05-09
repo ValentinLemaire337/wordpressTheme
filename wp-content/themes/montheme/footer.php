580bdd2e8fe0604681f8b0cd3ad946fa1c794dbe
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-05-09 15:04:29 +0200

    update

--- wp-content/themes/montheme/footer.php
+++ wp-content/themes/montheme/footer.php
@@ -1,58 +1,59 @@
 <div class="row">
     <img src="/wp-content/themes/montheme/img/Contrast 2.png" alt="mountain landscape">
 
-    <div class="footerArticles">
+    <div class="container">
         <h5>DECOUVREZ</h5>
         <h3>Articles les plus populaires</h3>
         <img src="/wp-content/themes/montheme/img/Shape 1.png" alt="shape">
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="/wp-content/themes/montheme/img/image 1.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div  class="footerArticles">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/wp-content/themes/montheme/img/image 1.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">Card title</h3>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div>
-            <div class="card" style="width: 18rem;">
-                <img src="/wp-content/themes/montheme/img/image 1.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/wp-content/themes/montheme/img/image 1.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">Card title</h3>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div>
-            <div class="card" style="width: 18rem;">
-                <img src="/wp-content/themes/montheme/img/image 1.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/wp-content/themes/montheme/img/image 1.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">Card title</h3>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div>
-            <div>
-                <h3>Accès rapide</h3>
-                <a href="">Accueil</a>
-                <a href="">Catégorie 1</a>
-                <a href="">Catégorie 2</a>
-                <a href="">Catégorie 3</a>
-            </div>
-            <div>
-                <h3>Contactez-nous</h3>
-                <p>70 rue des Jacobins</p>
-                <p>80000 Amiens</p>
-                <p>contactus@lamanu.fr</p>
-            </div>
-            <div>
-                <p>Inscrivez-vous à la newsletter</p>
             </div>
         </div>
     </div>
-</div>
+            <div class="infos">
+                <div>
+                    <h3>Accès rapide</h3>
+                    <a href="">Accueil</a>
+                    <a href="">Catégorie 1</a>
+                    <a href="">Catégorie 2</a>
+                    <a href="">Catégorie 3</a>
+                </div>
+                <div>
+                    <h3>Contactez-nous</h3>
+                    <p>70 rue des Jacobins</p>
+                    <p>80000 Amiens</p>
+                    <p>contactus@lamanu.fr</p>
+                </div>
+                <div>
+                    <p>Inscrivez-vous à la newsletter</p>
+                </div>
+            </div>
+
 
 
 <?php wp_footer(); ?>
