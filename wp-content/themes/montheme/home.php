a86cb6ce8458f86c7b1a7a16515c52f09a4047c4
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-05-10 12:07:28 +0200

    ff

--- wp-content/themes/montheme/home.php
+++ wp-content/themes/montheme/home.php
@@ -1,11 +1,9 @@
 <?php get_header(); ?>
- 	<h1>Le blog Capitaine WP</h1>
+ 	<h1>theme WP</h1>
 
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-  
 		<article class="post">
 			<h2><?php the_title(); ?></h2>
-      
         	<?php the_post_thumbnail(); ?>
             
             <p class="post__meta">
@@ -14,7 +12,7 @@
             </p>
             
       		<?php the_excerpt(); ?>
-              
+
       		<p>
                 <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
             </p>
