93767297ca97918aa92f7ee8ed19ac498a5a77de
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-05-10 15:45:24 +0200

    f

--- wp-content/themes/montheme/page.php
+++ wp-content/themes/montheme/page.php
@@ -1,8 +1,4 @@
-<?php get_header(); ?>
+<?php get_header() ?>
 
-<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-    <article class="">
-        <h2></h2>
-
-<?php get_footer(); ?>
+<?php get_footer() ?>
